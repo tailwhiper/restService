da6658d7f854b23ada86489269e1b9ec310db979
tailwhiper <tailwhiper@gmail.com>
2015-12-13 03:01:58 +0400

    finnally managed how does it work

---
+++ web/script.js
@@ -0,0 +1,23 @@
+function getAllMeetings() {
+    
+    var x = new XMLHttpRequest();
+
+    x.open("GET", "http://localhost:8080/meetings-service/meetings/full", true);
+    //x.setDisableHeaderCheck(true);
+    x.onload = function () {
+        alert(x.responseText);
+
+    }
+    x.send(null);
+
+}
+function putMeeting() {
+    var x = new XMLHttpRequest();
+
+    x.open("PUT", "http://localhost:8080/meetings-service/meetings/put/" + document.getElementById('title').value + "/" + document.getElementById('summary').value + "/" + document.getElementById('startdate').value + "/" + document.getElementById('enddate').value + "/" + document.getElementById('priority').value, true);
+
+    x.onload = function () {
+        alert(x.responseText);
+    }
+    x.send(null);
+}
